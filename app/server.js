cd295fe936da08d88acf1ff2db23c1d9a526aadf
Christopher W Schultz <chris.william.schultz@gmail.com>
2021-01-29 02:26:33 -0700

    restricted categories and recipes routes

--- app/server.js
+++ app/server.js
@@ -19,9 +19,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 // app routes
-app.use('/api/categories', categories);
-app.use('/api/recipes', recipes);
-// app.use('/api/recipes', restricted, recipes);
+app.use('/api/categories', restricted, categories);
+app.use('/api/recipes', restricted, recipes);
+// app.use('/api/categories', categories);
+// app.use('/api/recipes', recipes);
 
 // hello world route to verify the server runs
 app.get('/', (request, response, next) => {
