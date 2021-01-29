e977950bf8db3af4f0b477840c95b2466af4254f
Christopher W Schultz <chris.william.schultz@gmail.com>
2021-01-29 01:34:32 -0700

    changed to a ternary statement

--- app/middleware/restricted.js
+++ app/middleware/restricted.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (request, response, next) => {
     try {
-        let token = request.cookies.token || false;
+        let token = request.cookies.token ? request.cookies.token : false;
 
         if (token) {
             jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
