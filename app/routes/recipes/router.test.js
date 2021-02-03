acec85784ef86ed302636845d84e66b8d780cf1c
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-02-02 20:27:00 -0500

    recipes test

---
+++ app/routes/recipes/router.test.js
@@ -0,0 +1,19 @@
+const supertest = require("supertest")
+const db = require("../../../data/dbConfig")
+const server = require("../../server")
+
+beforeEach(async ()=> {
+    await db.seed.run()
+})
+
+afterAll(async ()=> {
+    await db.destroy()
+})
+
+describe("get recipes", ()=> {
+    it("doesn't get recipes", async ()=> {
+        const res = await supertest(server).get("/recipes")
+        expect(res.status).toBe(400)
+        expect(res.body.message).toBe("token required")
+    })
+})
